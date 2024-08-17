0495a2df4cee071f852d299c03df8a373de20faf
Asad Atik <asadatik1995@gmail.com>
2024-08-17 12:01:04 +0600

    get oparation in all All Product

--- index.js
+++ index.js
@@ -29,14 +29,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+      await client.connect();
+
+
+    const ProductCollection = client.db('JOB-TASK').collection('shopSort')
+    // get all product
+     
+     app.get('/allPro', async (req, res) => {
+      const result = await ProductCollection.find().toArray()
+
+      res.send(result)
+    }) 
+
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
@@ -46,9 +57,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Selling is Starting')
-  })
-  
-  app.listen(port, () => {
-    console.log(` Selling   is sitting on port ${port}`);
-  })
+  res.send('Selling is Starting')
+})
+
+app.listen(port, () => {
+  console.log(` Selling   is sitting on port ${port}`);
+})
